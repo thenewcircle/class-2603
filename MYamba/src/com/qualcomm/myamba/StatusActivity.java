3fca7ce2177b0c5f760944a1c2ac7d6996e6b3f0
Marko Gargenta <marko@marakana.com>
2012-08-15 12:32:26 -0700

    Added OnClickListener support

--- MYamba/src/com/qualcomm/myamba/StatusActivity.java
+++ MYamba/src/com/qualcomm/myamba/StatusActivity.java
@@ -1,15 +1,26 @@
 package com.qualcomm.myamba;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class StatusActivity extends Activity {
+public class StatusActivity extends Activity implements OnClickListener {
+	Button buttonUpdate;
+	EditText editStatus;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_status);
+        
+        buttonUpdate = (Button) findViewById(R.id.button_update);
+        editStatus = (EditText) findViewById(R.id.edit_status);
+        
+        buttonUpdate.setOnClickListener(this);
     }
 
     @Override
@@ -17,4 +28,11 @@ public class StatusActivity extends Activity {
         getMenuInflater().inflate(R.menu.activity_status, menu);
         return true;
     }
+
+
+    // Callback from OnClickListener
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		
+	}
 }
