413ae3bb7215c41594b4d044313a2f42a43d07a2
Marko Gargenta <marko@marakana.com>
2012-08-15 14:10:16 -0700

    Implemented button events

--- MYamba/src/com/qualcomm/myamba/StatusActivity.java
+++ MYamba/src/com/qualcomm/myamba/StatusActivity.java
@@ -2,6 +2,7 @@ package com.qualcomm.myamba;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -12,27 +13,33 @@ public class StatusActivity extends Activity implements OnClickListener {
 	Button buttonUpdate;
 	EditText editStatus;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_status);
-        
-        buttonUpdate = (Button) findViewById(R.id.button_update);
-        editStatus = (EditText) findViewById(R.id.edit_status);
-        
-        buttonUpdate.setOnClickListener(this);
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_status);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_status, menu);
-        return true;
-    }
+		buttonUpdate = (Button) findViewById(R.id.button_update);
+		editStatus = (EditText) findViewById(R.id.edit_status);
 
+		buttonUpdate.setOnClickListener(this);
+	}
 
-    // Callback from OnClickListener
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_status, menu);
+		return true;
+	}
+
+	// Callback from OnClickListener
 	public void onClick(View v) {
-		// TODO Auto-generated method stub
-		
+		switch (v.getId()) {
+		case R.id.button_update:
+			String status = editStatus.getText().toString();
+			Log.d("Yamba", "onClick'ed with status " + status);
+			break;
+		case R.id.button_clear:
+			editStatus.setText("");
+			break;
+		}
 	}
 }
