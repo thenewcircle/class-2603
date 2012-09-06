332940a20a9bf425e6012db08ce82a896955f712
Marko Gargenta <marko@marakana.com>
2012-09-06 12:00:40 -0700

    Implemented Compass

--- Compass/src/com/qualcomm/compass/MainActivity.java
+++ Compass/src/com/qualcomm/compass/MainActivity.java
@@ -6,6 +6,7 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class MainActivity extends Activity implements SensorEventListener {
@@ -24,12 +25,17 @@ public class MainActivity extends Activity implements SensorEventListener {
 		// Sensor stuff
 		sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
 		sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION);
+		
+		for( Sensor s: sensorManager.getSensorList(Sensor.TYPE_ALL) ) {
+			Log.d("Compass", "Sensor: "+ s.getName());
+		}
 	}
 
 	@Override
 	protected void onResume() {
 		super.onResume();
-		sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_UI);
+		sensorManager.registerListener(this, sensor,
+				SensorManager.SENSOR_DELAY_UI);
 	}
 
 	@Override
@@ -39,14 +45,15 @@ public class MainActivity extends Activity implements SensorEventListener {
 	}
 
 	// --- SensorEventListener callbacks ---
-	
-	@Override
-	public void onAccuracyChanged(Sensor sensor, int accuracy) {
-	}
-
 	@Override
 	public void onSensorChanged(SensorEvent event) {
 		// TODO: Update output with azimuth value reported by the sensor event
+		output.setText(String.format("%d azimuth\n%d pitch\n%d roll",
+				(int) event.values[0], (int) event.values[1],
+				(int) event.values[2]));
 	}
 
+	@Override
+	public void onAccuracyChanged(Sensor sensor, int accuracy) {
+	}
 }
