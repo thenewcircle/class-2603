716bd7625f99fa268de44e69e2ff0015e7ba6dd6
Marko Gargenta <marko@marakana.com>
2012-09-06 11:33:19 -0700

    Added SensorListener

--- Compass/src/com/qualcomm/compass/MainActivity.java
+++ Compass/src/com/qualcomm/compass/MainActivity.java
@@ -1,23 +1,52 @@
 package com.qualcomm.compass;
 
 import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements SensorEventListener {
 	TextView output;
-	SensorManager manager;
-	
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
-        output = (TextView) findViewById(R.id.output);
-        
-        manager = (SensorManager) getSystemService(SENSOR_SERVICE);
-    }
+	SensorManager sensorManager;
+	Sensor sensor;
 
-    
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		// UI stuff
+		setContentView(R.layout.activity_main);
+		output = (TextView) findViewById(R.id.output);
+
+		// Sensor stuff
+		sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+		sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION);
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_UI);
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		sensorManager.unregisterListener(this);
+	}
+
+	// --- SensorEventListener callbacks ---
+	
+	@Override
+	public void onAccuracyChanged(Sensor sensor, int accuracy) {
+	}
+
+	@Override
+	public void onSensorChanged(SensorEvent event) {
+		// TODO: Update output with azimuth value reported by the sensor event
+	}
+
 }
